648dcd08f1fdae64ac2cb03ea6edf40b67963e6a
vdigmJiMin <andy@vdigm.com>
2024-03-14 21:41:57 +0900

    a

---
+++ TIHStation/Plugins/TIHPakBox/TIHHousingPak/Intermediate/Build/Win64/UnrealEditor/Inc/TIHHousingPak/UHT/TIHHousingPakCore.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "TIHHousingPakCore.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TIHHOUSINGPAK_TIHHousingPakCore_generated_h
+#error "TIHHousingPakCore.generated.h already included, missing '#pragma once' in TIHHousingPakCore.h"
+#endif
+#define TIHHOUSINGPAK_TIHHousingPakCore_generated_h
+
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_SPARSE_DATA
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_ACCESSORS
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATIHHousingPakCore(); \
+	friend struct Z_Construct_UClass_ATIHHousingPakCore_Statics; \
+public: \
+	DECLARE_CLASS(ATIHHousingPakCore, ATIHPakBase, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TIHHousingPak"), NO_API) \
+	DECLARE_SERIALIZER(ATIHHousingPakCore)
+
+
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATIHHousingPakCore(); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATIHHousingPakCore(ATIHHousingPakCore&&); \
+	NO_API ATIHHousingPakCore(const ATIHHousingPakCore&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATIHHousingPakCore); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATIHHousingPakCore); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATIHHousingPakCore) \
+	NO_API virtual ~ATIHHousingPakCore();
+
+
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_12_PROLOG
+#define FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_SPARSE_DATA \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_ACCESSORS \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TIHHOUSINGPAK_API UClass* StaticClass<class ATIHHousingPakCore>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Projects_Rapid_Project_TIH_20231011_TIHStation_Plugins_TIHPakBox_TIHHousingPak_Source_TIHHousingPak_Public_TIHHousingPakCore_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
